42e8235c193c71132afa25ae683f70cd84c10f16
DrSoftman <168275864+DrSoftman@users.noreply.github.com>
2025-08-16 15:59:13 +0200

    Create spel.h

---
+++ include/spel.h
@@ -0,0 +1,20 @@
+#ifndef SPEL_H
+#define SPEL_H
+
+#define DEFAULT_WIDTH 800
+#define DEFAULT_HEIGHT 600
+
+#define PLAYER_WIDTH 90
+#define PLAYER_HEIGHT 100
+#define PLAYER_SPEED 6
+#define JUMP_FORCE 15
+#define GRAVITY 1
+
+#define CAMERA_SMOOTHING 0.05f
+#define CAMERA_DEADZONE 150
+
+#define WALK_FRAME_COUNT 2
+#define ANIMATION_SPEED 10
+
+
+#endif // SPEL_H
